35ce7112ea7b3903a94a53360cf4bad40c9e4580
Gerard Ketuma <gketuma@gmail.com>
2014-02-14 19:41:46 -0600

    Fixed issue with the font files not copying over

--- Gruntfile.js
+++ Gruntfile.js
@@ -306,7 +306,8 @@ module.exports = function (grunt) {
                         'images/{,*/}*.webp',
                         '{,*/}*.html',
                         'styles/fonts/{,*/}*.*',
-                        'bower_components/' + (this.includeCompass ? 'sass-' : '') + 'bootstrap/' + (this.includeCompass ? 'fonts/' : 'dist/fonts/') +'*.*'
+                        'bower_components/' + (this.includeCompass ? 'sass-' : '') + 'bootstrap/' + (this.includeCompass ? 'fonts/' : 'dist/fonts/') +'*.*',
+			'fonts/'
                     ]
                 }]
             },
